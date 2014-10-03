be7c8cef3663ccf86d40436c91c28e7700c3030c
Alex von Hafften <Kerstin@kitte-millers-macbook-pro.local>
2014-10-03 10:00:43 -0700

    My app now saves data to a sqlite3 instead of a text file

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 ruby "2.1.1"
 source 'https://rubygems.org'
-gem 'sinatra'
-gem 'sqlite3'
-gem 'shotgun'
+gem 'sinatra', '>= 1.4.5'
+gem 'sqlite3', '>= 1.3.9'
+gem 'shotgun', '>= 0.9'
+gem 'activerecord', '4.0.4'
+gem 'sinatra-activerecord', require: 'sinatra/activerecord'
+gem 'rake'
+gem 'tux'
--- app.rb
+++ app.rb
@@ -2,34 +2,24 @@
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require
+require './models/TodoItem'
+
+# how we want to wire the database
+ActiveRecord::Base.establish_connection(
+	adapter: 'sqlite3',
+	database: 'db/development.db',
+	encoding: 'utf8'
+)
 
 get '/' do
-	file_contents = File.read('todo.txt')
-	lines = file_contents.split("\n")
-	@tasks = []
-	@undated_tasks = []
-	@dated_tasks = []
-	lines.each do |line|
-		task, date = line.split("-")
-		unless blank? date
-			@dated_tasks << [task, date]
-		else
-			@undated_tasks << [task, date]
-		end
-	end
-	@tasks = @dated_tasks.push(*@undated_tasks)
+	# need to interact with the database
+	@task = TodoItem.all.order(:due_date)
 	erb :index
 end
 
 post '/' do
-	File.open("todo.txt", "a") do |file|
-		if blank? params[:date]
-			file.puts "#{params[:task]}"
-		else
-			file.puts "#{params[:task]} - #{params[:date]}"
-		end
-		redirect '/'
-	end
+	TodoItem.create(params)
+	redirect '/'
 end
 
 helpers do
