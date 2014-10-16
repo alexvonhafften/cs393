b1e5b341ae704b985489ff7cd7eea84a4ad367b3
Alex von Hafften <Kerstin@kitte-millers-macbook-pro.local>
2014-10-16 12:29:38 -0700

    Set up app to use postgrespl in production

--- app.rb
+++ app.rb
@@ -4,12 +4,15 @@ require 'bundler/setup'
 Bundler.require
 require './models/TodoItem'
 
-# how we want to wire the database
-ActiveRecord::Base.establish_connection(
-	adapter: 'sqlite3',
-	database: 'db/development.db',
-	encoding: 'utf8'
-)
+if ENV['DATABASE_URL']
+	ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+	ActiveRecord::Base.establish_connection(
+		:adapter =>'sqlite3',
+		:database => 'db/development.db'
+		:encoding => 'utf8'
+	)
+end
 
 get '/' do
 	# need to interact with the database
